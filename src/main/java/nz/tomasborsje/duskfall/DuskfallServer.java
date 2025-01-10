800fe0014cfa53e55e94f38e54c722b6b2a342da
Tomas Borsje <borsjetoma@myvuw.ac.nz>
2025-01-10 14:54:57 +1300

    feat: Custom server MOTD, icon, player count

--- src/main/java/nz/tomasborsje/duskfall/DuskfallServer.java
+++ src/main/java/nz/tomasborsje/duskfall/DuskfallServer.java
@@ -16,6 +16,7 @@ import nz.tomasborsje.duskfall.database.DatabaseConnection;
 import nz.tomasborsje.duskfall.events.EntityMeleeAttackListener;
 import nz.tomasborsje.duskfall.events.PlayerJoinListener;
 import nz.tomasborsje.duskfall.events.PlayerLeaveListener;
+import nz.tomasborsje.duskfall.events.ServerListPingListener;
 import nz.tomasborsje.duskfall.registry.ItemRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -98,6 +99,7 @@ public class DuskfallServer {
         //globalEventHandler.addListener(new BlockBreakListener());
         globalEventHandler.addListener(new PlayerLeaveListener());
         globalEventHandler.addListener(new EntityMeleeAttackListener());
+        globalEventHandler.addListener(new ServerListPingListener());
     }
 
     private static void registerCommands(CommandManager commandManager) {
---
+++ src/main/java/nz/tomasborsje/duskfall/events/ServerListPingListener.java
@@ -0,0 +1,54 @@
+package nz.tomasborsje.duskfall.events;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.Style;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.minestom.server.event.EventListener;
+import net.minestom.server.event.server.ServerListPingEvent;
+import net.minestom.server.ping.ResponseData;
+import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Base64;
+
+public class ServerListPingListener implements EventListener<ServerListPingEvent> {
+
+    private static final Component motdTopLine =
+            Component.text(StringUtils.center("Duskfall", 45-"v0.0.1".length()), NamedTextColor.LIGHT_PURPLE, TextDecoration.BOLD)
+            .append(Component.text("v0.0.1", Style.style().color(NamedTextColor.WHITE).decoration(TextDecoration.BOLD, false).build()))
+            .appendNewline()
+            .append(Component.text(StringUtils.center("-- Dev Test --", 45), NamedTextColor.BLUE));
+
+    private final String serverIconBase64;
+
+    public ServerListPingListener() {
+        try {
+            byte[] pngByteArray = Files.readAllBytes(Path.of("server-icon.png"));
+            serverIconBase64 = Base64.getEncoder().encodeToString(pngByteArray);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @NotNull
+    @Override
+    public Result run(@NotNull ServerListPingEvent event) {
+        ResponseData responseData = new ResponseData();
+        responseData.setMaxPlayer(1000);
+        responseData.setOnline(999);
+        responseData.setDescription(motdTopLine);
+        responseData.setFavicon(serverIconBase64);
+
+        event.setResponseData(responseData);
+        return Result.SUCCESS;
+    }
+
+    @Override
+    public @NotNull Class<ServerListPingEvent> eventType() {
+        return ServerListPingEvent.class;
+    }
+}
